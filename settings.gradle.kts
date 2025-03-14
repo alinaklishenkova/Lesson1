d774b5b7f9a709f3c6aaccbb8e93619259e79adf
alina <alinaklishenkova48@gmail.com>
2025-03-14 17:16:34 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lesson1"
+include(":app")
+include(":layouttype")
+include(":control_lesson1")
+include(":number_five")
+include(":number_six")
+include(":number_six2")
+include(":buttonclicker")
